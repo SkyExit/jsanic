8be3482c36ae7ab7c3529e6c0c4832727963ca68
BuildTools <unconfigured@null.spigotmc.org>
2020-08-15 18:42:20 +0200

    v1

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const JsanicClient = require('./Structures/JsanicClient');
-const config = require('./config.json');
+const config = require('./cConfig.json');
 
 const client = new JsanicClient(config);
 client.start();
